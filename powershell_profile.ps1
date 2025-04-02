a55696244a82982fa23ea002784f7163719b917a
MatthewGadsden <matthewjgadsden@gmail.com>
2025-04-02 22:06:06 +1100

    feat: add git-push-create command

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,2 +1,6 @@
 oh-my-posh.exe init pwsh --config "C:\Configurations\ps1\gaddo.omp.json" | Invoke-Expression
 Import-Module posh-git # more details https://github.com/dahlbyk/posh-git
+
+function git-pc() {
+	git rev-parse --abbrev-ref HEAD | git push -u origin
+}
